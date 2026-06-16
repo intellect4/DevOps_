5c981dd84b91cde4b441b6e2c586da65297a1106
intellect4 <shreyas.s3105@gmail.com>
2026-06-16 11:46:43 +0530

    first commit

--- src/test/java/org/test/WebPageTest.java
+++ src/test/java/org/test/WebPageTest.java
@@ -1,4 +1,41 @@
 package org.test;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
 public class WebPageTest {
+
+    WebDriver driver;
+
+    @BeforeTest
+    public void openBrowser() {
+
+        driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.get("https://intellect4.github.io/DevOps_/");
+    }
+
+    @Test
+    public void titleTest() {
+
+        String actual = driver.getTitle();
+
+        String expected = "Webpage Design";
+
+
+
+        Assert.assertEquals(actual, expected);
+
+    }
+
+    @AfterTest
+    public void closeBrowser() {
+
+        driver.quit();
+
+    }
 }
